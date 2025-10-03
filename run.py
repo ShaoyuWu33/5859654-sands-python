99aa5398e650eaf74916e0a154e621dd7bbb44fc
Your Name <wushaoyu0220@gmail.com>
2025-10-03 19:46:00 +0200

    initial preparation

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-from signals import generate_sine_wave
+import os
 import matplotlib.pyplot as plt
-
-sine_wave = generate_sine_wave(frequency=5, duration=2, sample_rate=100)
-# Create time points for plotting
-t = np.linspace(0, 2, int(2 * 100))
-
-plt.plot(t, sine_wave)
-plt.title('Sine Wave (5 Hz)')
-plt.xlabel('Time (seconds)')
-plt.ylabel('Amplitude')
+import numpy as np
+from signals import generate_sinusoidal, generate_step, time_shift, time_scale
