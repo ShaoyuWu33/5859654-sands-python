40e42d3bb2c2e19bc595db13763e42ba4b12bc6d
Your Name <wushaoyu0220@gmail.com>
2025-10-03 18:50:22 +0200

    time scale

--- signals.py
+++ signals.py
@@ -2,7 +2,7 @@
 Includes at least two signal generators and two signal transformations
 import numpy as np
 def generate_sinusoidal_signal(frequency, duration, sampling_rate, amplitude, phase):
-    t = np.linspace(0, duration, int(sampling_rate * duration, endpoint=False))
+    t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
     x = amplitude * np.sin(2 * np.pi * frequency * t + phase)
     return t, x
 
@@ -18,4 +18,11 @@ def time_shift(signal: np.array, shift):
         shifted_signal = np.concatenate((signal[-shift:], np.zeros(-shift)))
     else:
         shifted_signal = signal
-    return shifted_signal.
+    return shifted_signal
+
+def time_scale(signal: np.array, scale):
+    n = np.arange(len(signal))
+    new_length = int(len(signal) / scale)
+    new_n = np.linspace(0, len(signal) - 1, new_length)
+    scaled_signal = np.interp(new_n, n, signal)
+    return scaled_signal.
