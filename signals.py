534ae121a774c6bf37b6ffe3e77136d3395afc91
Your Name <wushaoyu0220@gmail.com>
2025-10-03 16:46:07 +0200

    sinusoidal signal

---
+++ signals.py
@@ -0,0 +1,4 @@
+# Contains functions for signal generation and signal operations. 
+Includes at least two signal generators and two signal transformations
+import numpy as np
+def generate_sinusoidal(frequency, duration, sampling_rate, amplitude, phase):.
