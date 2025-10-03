005211c602f79e6a87141f458c3ec5e9a5f91cad
Your Name <wushaoyu0220@gmail.com>
2025-10-03 21:12:02 +0200

    transformations: time shift and time scaling

--- run.py
+++ run.py
@@ -11,4 +11,15 @@ phase = 0          # radians
 t, sinusoid = generate_sinusoidal_signal(freq, duration, sampling_rate, amplitude, phase)
 # Unit step signal parameter
 length = 50
-n, step_signal = generate_unit_step(length)ale
+n, step_signal = generate_unit_step(length)
+
+# 2. Apply Transformations
+# Time shift
+shift_samples = 50
+shifted_sinusoid = time_shift(sinusoid, shift_samples)
+shifted_step = time_shift(step_signal, 5)
+
+# Time scaling
+scale_factor = 0.5  
+scaled_sinusoid = time_scale(sinusoid, scale_factor)
+scaled_step = time_scale(step_signal, 2)  ale
